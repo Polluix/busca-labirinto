b9f5e7e21823f5130c88eceae0b011d2f2fe49d3
Polluix <lipe311973@gmail.com>
2024-04-14 19:21:26 -0300

    busca em largura implementada

--- busca_labirinto/__init__.py
+++ busca_labirinto/__init__.py
@@ -1,11 +1,13 @@
 from pyamaze import maze, agent
 from random import randint, choice
+import time
 
 class No():
     def __init__(self) -> None:
         self.coord: tuple = () #coordenada da célula no labirinto
         self.nos: list = [] #coordenada das células que são caminhos válidos a partir do nó atual
         self.anterior: object = None #coordenada da célula visitada anteriormente no labirinto
+        self.visitado: bool = False #se foi visitado ou nao
 
     def setCoord(self, coord:tuple) -> None:
         
@@ -53,11 +55,19 @@ def createMaze(size=10):
         line = 1
         column = randint(1, eval(size))
 
-    lab.CreateMaze(line, column,loopPercent=15)
+    lab.CreateMaze(line, column,
+                   loopPercent=0.5,
+                   )
     
     agente = agent(lab)
 
     return lab, agente, line, column
+
+def marca_visitado(node):
+    node.visitado = True
+    return node
+
+inicio = time.time()
 
 size = input('Tamanho do labirinto: ')
 
@@ -75,36 +85,101 @@ initial_node.setCoord((initial_line, initial_column))
 initial_node.criaNos()
 
 # TODO1 : arrumar lógica da avaliação de cada nó
-
+#* busca em profundidade
 nodes = initial_node.nos
-i = 0
+# i = 0
+# while initial_column!=column and initial_line!=line: #verifica se não está na saída
+    
+#     for node in nodes:  #itera pelos nos mais profundos a cada passo
+#         coord_line, coord_column = node.coord
+
+#         if coord_line!=line and coord_column!=column: #verifica se não são as coordenadas da saída
+#             node.criaNos()                             #cria os nós filhos para o próximo passo
+#             for children in node.nos:
+#                 children.setAnterior(node)
+
+#         elif coord_line==line and coord_column==column:
+#             initial_column = coord_column
+#             initial_line = coord_line
+    
+#     new = []
+#     for node in nodes:
+#         for node in node.nos:
+#             new.append(node)
+#     nodes.clear()
+#     nodes = new
+#     [print(i.coord) for i in nodes]
+#     break
+
+#* busca em largura
+# acessa nós
+# le coordenada
+# se for saída, para o laço
+# se não for saída, criaNos()
+#acesa a lista de nós, abrindo um por um, criando nós enquanto nao chegar na saída
+    # acessa no, verifica saida, senao, cria nos, volta pro anterior (usando atributo da classe)
+
+i = 0 # controla a iteração de acesso aos nos
+
 while initial_column!=column and initial_line!=line: #verifica se não está na saída
     
-    for node in nodes:  #itera pelos nos mais profundos a cada passo
-        coord_line, coord_column = node.coord
+    if i==0:
+        for node in nodes:
+            coord_line, coord_column = node.coord
+            node.visitado = True
+            # print(coord_line, coord_column)
 
-        if coord_line!=line and coord_column!=column: #verifica se não são as coordenadas da saída
             node.criaNos()                             #cria os nós filhos para o próximo passo
             for children in node.nos:
                 children.setAnterior(node)
+    else:
+        for j in range(len(nodes)):
+            node = nodes[j]
+            if node.visitado==False:
+                node.visitado = True
+                coord_line, coord_column = node.coord
+                # print(coord_line, coord_column)
+                
+                if coord_line!=line or coord_column!=column: #verifica se não são as coordenadas da saída
+                    print('Não é a saída')
+                    node.criaNos()                             #cria os nós filhos para o próximo passo
+                    # print('nao sao iguais')
+                    for children in node.nos:
+                        children.setAnterior(node)
 
-        elif coord_line==line and coord_column==column:
-            initial_column = coord_column
-            initial_line = coord_line
-    
+                elif coord_line==line and coord_column==column:
+                    print('É a saída')
+                    # print('sao iguais')
+                    while node!=None:
+                        visitados.append(node.coord)
+                        node = node.anterior
+                        
+                    tupla_coord = (eval(size),eval(size))
+                    visitados.append(tupla_coord)
+                    initial_column=column
+                    initial_line=line
+                    break
+
+        n_nos = []
+        n_nos.append(len(nodes))
+
     new = []
     for node in nodes:
-        for node in node.nos:
-            new.append(node)
+        for no in node.nos:
+            new.append(no)
+    
     nodes.clear()
     nodes = new
-    [print(i.coord) for i in nodes]
-    break
-
+    new = []
+    i+=1
+fim = time.time()
+print(visitados)
+print(f'tempo de busca: {fim-inicio:.2f} s')
+print(f'nós avaliados: {n_nos}')
+# print(lab.path)
 lab.run()
 
-print(coord_line, coord_column)
-print(line, column)
+
 
 # TODO2: A solução deve conter as coordenadas de cada célula do caminho a ser percorrido
 # TODO2: e deve ser comparada com a solução ótima proposta pelo próprio pyamaze
